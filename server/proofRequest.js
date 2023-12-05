d82c275b364a18e69c5ee86ecedfe384edd05d34
xiaodong.yu <yuxiaodongnju@outlook.com>
2023-12-05 17:58:36 +0800

    edit  ignore and request

--- server/proofRequest.js
+++ server/proofRequest.js
@@ -1,26 +1,27 @@
 const { KYCAgeCredential } = require("./vcHelpers/KYCAgeCredential");
 const { zkKYCPassport } = require("./vcHelpers/zkKYCPassport");
 
-// https://devs.polygonid.com/docs/verifier/verification-library/zk-query-language/
+require("dotenv").config();
 
-const humanReadableAuthReason = "Must be born before this year";
-
-const credentialSubjectAge = {
-  birthday: {
-    // users must be born before this year
-    // birthday is less than Jan 1, 2023
-    $lt: 20230101,
-  },
-};
+const humanReadableAuthReason = process.env.AUTH_REASON;
 
 const credentialSubject = {
-  DateOfBirth: {
-    $lt: 20230101,
+  birthday: {
+    $lt: 20231201,
   },
 };
 
-// const proofRequest = KYCAgeCredential(credentialSubjectAge);
-const proofRequest = zkKYCPassport(credentialSubject);
+const proofRequest = KYCAgeCredential(credentialSubject);
+
+//------------------------------------------------------------
+
+// const credentialSubject = {
+//   Sex: {
+//     $eq: "Female",
+//   },
+// };
+
+// const proofRequest = zkKYCPassport(credentialSubject);
 
 module.exports = {
   humanReadableAuthReason,
